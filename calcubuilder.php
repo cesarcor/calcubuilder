d427e073ef488b9ff4d273b3fdd2d7345d98bcd7
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2016-12-01 19:24:14 -0600

    First Upload and commit
    Directory structure is being concocted and plugin headers have been
    added.

---
+++ calcubuilder.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Calcubuilder
+Plugin URI:
+Description: WordPress Calculator Builder
+Version:     0.1
+Author:      Cesar Correchel Downs
+Author URI:  http://www.cesardowns.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wporg
+*/
