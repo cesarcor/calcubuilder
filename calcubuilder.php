33c686c7a24096624c7693c9dc65a0419ca49e6b
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-01-07 14:10:24 -0600

    Constants Added
    **plugin still in planning.

--- calcubuilder.php
+++ calcubuilder.php
@@ -11,6 +11,11 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: wporg
 */
 
+// -- plugin's constants --
+
 define('CLCBUILDER_VERSION', '0.1');
 define('CLCBUILDER_REQUIREDWP_VERSION', '4.6');
-define('CLCBUILDER_PLUGIN_DIR', '');
+define('CLCBUILDER_PLUGIN', __FILE__ );
+define('CLCBUILDER_PLUGIN_BASENAME', plugin_basename( CLCBUILDER_PLUGIN ) );
+define('CLCBUILDER_PLUGIN_NAME', trim( dirname( CLCBUILDER_PLUGIN_BASENAME ), '/' ) );
+define('CLCBUILDER_PLUGIN_DIR', untrailingslashit( dirname( CLCBUILDER_PLUGIN ) ));
