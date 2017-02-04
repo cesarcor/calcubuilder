e8d4d68775c6fa9176112aaf2e7c90739d4d18b0
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-02-04 01:34:27 -0600

    add code for activation hook

---
+++ includes/calcubuilder-activator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * @link       http://www.cesardowns.com
+ * @since      0.1
+ *
+ * @package    calcubuilder
+ * @subpackage calcubuilder/includes
+ */
+
+ class Calcubuilder_Activator{
+
+   public static function activate() {
+
+   }
+
+
+ }
