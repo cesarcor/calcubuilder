15c5e80e8d05fc847369607fd53e56d031ddb9e1
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-02-04 01:32:54 -0600

    new calculator view
    testing container for new calculator view.

--- admin/views/new-calc.php
+++ admin/views/new-calc.php
@@ -1,6 +1,13 @@
-<h1>New Calculator</h1>
+<h1 class="pg">New Calculator</h1>
 
-<div>
+<!-- New Calculator Screen -->
+<div class="container">
+  <div class="clcbuilder_menu">
+    <ul>
+      <li>Operations</li>
+      <li>Compound Operations</li>
+      <li>Grouping</li>
+    </ul>
+  </div>
 
-
-<div>
+</div>
