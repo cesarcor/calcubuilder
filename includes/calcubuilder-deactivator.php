b9bfeca68f745485590e88d7c89c8d900dc9a2b5
Cesar C. Downs <cesarcdowns@Cesars-MacBook-Pro.local>
2017-02-04 01:35:19 -0600

    plugin code for deactivation hook

---
+++ includes/calcubuilder-deactivator.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * @link       http://www.cesardowns.com
+ * @since      0.1
+ *
+ * @package    calcubuilder
+ * @subpackage calcubuilder/includes
+ */
+
+ class Calcubuilder_Deactivator{
+
+   public static function deactivate() {
+
+   }
+
+
+ }
